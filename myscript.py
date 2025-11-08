ff70f5fba219c2a064d15eb0c55e2a012ba06455
RabahN <144280739+RabahN@users.noreply.github.com>
2025-11-08 17:00:05 -0500

    correction script

--- myscript.py
+++ myscript.py
@@ -1,6 +1,7 @@
 import os
+import sys
 BAD_HASH = "badhash"
 GOOD_HASH = "goodhash"
 os.system(f"git bisect start {BAD_HASH} {GOOD_HASH}")
-os.system("git bisect run python -m pytest")
-os.system("git bisect reset")
+exit_code = os.system("python -m pytest > result.log 2>&1")
+sys.exit(exit_code)
