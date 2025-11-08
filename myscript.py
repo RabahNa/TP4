ed0781f4e07b3acaaa7e6e2f2b26a6367bc8bc28
RabahN <144280739+RabahN@users.noreply.github.com>
2025-11-08 17:18:36 -0500

    modif mineur script

--- myscript.py
+++ myscript.py
@@ -1,7 +1,6 @@
 import os
-import sys
 BAD_HASH = "c1a4be04b972b6c17db242fc37752ad517c29402"
 GOOD_HASH = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
 os.system(f"git bisect start {BAD_HASH} {GOOD_HASH}")
-exit_code = os.system("python manage.py test > result.log 2>&1")
-sys.exit(exit_code)
+os.system("git bisect run python -m pytest")
+os.system("git bisect reset")
