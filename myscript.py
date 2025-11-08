95a408d30bca1941e546e5b84303ee307889d484
RabahN <144280739+RabahN@users.noreply.github.com>
2025-11-08 17:03:11 -0500

    correction script 2

--- myscript.py
+++ myscript.py
@@ -3,5 +3,5 @@ import sys
 BAD_HASH = "badhash"
 GOOD_HASH = "goodhash"
 os.system(f"git bisect start {BAD_HASH} {GOOD_HASH}")
-exit_code = os.system("python -m pytest > result.log 2>&1")
+exit_code = os.system("python manage.py test > result.log 2>&1")
 sys.exit(exit_code)
